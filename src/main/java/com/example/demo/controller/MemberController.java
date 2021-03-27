8f3a6b0e6b0e55a14b1c882d452a9428fef11032
kbpark9898 <kbpark9898@gmail.com>
2021-03-27 16:50:41 +0900

    0327_member register function

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // git config change!
 @Controller
@@ -12,5 +15,18 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping(value ="/members/new")
+    public String createRegisterForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String memberRegister(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberService.join(member);
+        return "redirect:/";
     }
 }
